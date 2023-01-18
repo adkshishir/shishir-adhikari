beaf502b759935ec955b496276f19bc57a3eb073
adkshishir <98789290+adkshishir@users.noreply.github.com>
2023-01-18 18:03:15 +0545

    nothing

--- index.js
+++ index.js
@@ -25,12 +25,14 @@ function toggleFunction() {
   </svg>`;
 
     mainNav.style.transform = "translateX(100vw)";
+
     toggle.style.transform = "translateX(-100vw)";
   } else {
     toggle.innerHTML = `
     <svg width="32" height="31" xmlns="http://www.w3.org/2000/svg"><g fill="#00001A" fill-rule="evenodd"><path d="m2.919.297 28.284 28.284-2.122 2.122L.797 2.419z"/><path d="M.797 28.581 29.081.297l2.122 2.122L2.919 30.703z"/></g></svg>
     `;
     mainNav.style.transform = "translateX(0)";
+
     toggle.style.transform = "translateX(0)";
   }
   navActive = !navActive;
