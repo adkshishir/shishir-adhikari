968f2f2d4f590c23d637e8dee0f0b0538597788f
adkshishir <98789290+adkshishir@users.noreply.github.com>
2023-01-18 18:48:07 +0545

    hack

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ function toggleFunction() {
     </g>
   </svg>`;
 
-    mainNav.style.transform = "translateX(100vw)";
+    mainNav.style.transform = "translateX(-100vw)";
 
-    toggle.style.transform = "translateX(-100vw)";
+    toggle.style.transform = "translateX(100vw)";
   } else {
     toggle.innerHTML = `
     <svg width="32" height="31" xmlns="http://www.w3.org/2000/svg"><g fill="#00001A" fill-rule="evenodd"><path d="m2.919.297 28.284 28.284-2.122 2.122L.797 2.419z"/><path d="M.797 28.581 29.081.297l2.122 2.122L2.919 30.703z"/></g></svg>
@@ -42,8 +42,8 @@ window.onresize = function () {
   if (window.innerWidth > 900) {
     mainNav.style.transform = "translateX(0)";
   } else {
-    mainNav.style.transform = "translateX(100vw)";
-    toggle.style.transform = "translateX(-100vw)";
+    mainNav.style.transform = "translateX(-100vw)";
+    toggle.style.transform = "translateX(100vw)";
     toggle.innerHTML = ` <svg width="40" height="17" xmlns="http://www.w3.org/2000/svg">
     <g fill="#00001A" fill-rule="evenodd">
       <path d="M0 0h40v3H0zM0 7h40v3H0zM0 14h40v3H0z" />
